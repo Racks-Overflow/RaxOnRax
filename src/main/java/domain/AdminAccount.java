a89558b1d7315b9fa50c50e3ac8dc0a2f0750c6a
NicholasWolak <nick.wolak@hotmail.com>
2021-08-24 18:01:22 -0400

    (feat:nick) added constructors to domains

--- src/main/java/domain/AdminAccount.java
+++ src/main/java/domain/AdminAccount.java
@@ -7,7 +7,41 @@ import java.util.List;
 @Entity
 public class AdminAccount extends Account{
 
-    List<GuestAccount> tripGroup;
+    private List<GuestAccount> tripGroup;
 
-    List<Account> copayees;
+    private List<Account> copayees;
+
+    public AdminAccount() {
+    }
+
+    public AdminAccount(Long accountNum, String username,
+                        String email, int phoneNumber,
+                        String address, double balance,
+                        Boolean tripStatus, Boolean permission,
+                        List<Trip> trips, List<GuestAccount> tripGroup,
+                        List<Account> copayees) {
+        super(accountNum, username,
+                email, phoneNumber,
+                address, balance,
+                tripStatus, permission,
+                trips);
+        this.tripGroup = tripGroup;
+        this.copayees = copayees;
+    }
+
+    public List<GuestAccount> getTripGroup() {
+        return tripGroup;
+    }
+
+    public void setTripGroup(List<GuestAccount> tripGroup) {
+        this.tripGroup = tripGroup;
+    }
+
+    public List<Account> getCopayees() {
+        return copayees;
+    }
+
+    public void setCopayees(List<Account> copayees) {
+        this.copayees = copayees;
+    }
 }
