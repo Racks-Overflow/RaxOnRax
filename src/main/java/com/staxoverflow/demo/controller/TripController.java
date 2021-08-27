a27decea9ad2962bcca06cb5900e11e050e81573
NicholasWolak <nick.wolak@hotmail.com>
2021-08-27 15:48:55 -0400

    (feat:nick) reintegrated Trip Controller/AccountController

--- src/main/java/com/staxoverflow/demo/controller/TripController.java
+++ src/main/java/com/staxoverflow/demo/controller/TripController.java
@@ -53,11 +53,14 @@ public class TripController {
         return new ResponseEntity<>(tripService.addGuest(tripId, newGuest), HttpStatus.OK);
     }
 
-    @PutMapping(value = "/{trip_id}/invite/{id}")
-    public ResponseEntity<Trip> assignAdmin(@PathVariable Long tripId, @PathVariable Long accountId){
-        Account newGuest = accountService.read(accountId);
-        return new ResponseEntity<>(tripService.addGuest(tripId, newGuest), HttpStatus.OK);
-    }
+//    @PutMapping(value = "/{trip_id}/invite/{id}")
+//    public ResponseEntity<Trip> assignAdmin(
+//            @PathVariable Long tripId,
+//            @PathVariable Long accountId){
+//        Account newGuest = accountService.read(accountId);
+//        return new ResponseEntity<>(tripService.addGuest(tripId, newGuest),
+//                HttpStatus.OK);
+//    }
 
     @PutMapping(value = "/{trip_id}/remove/{id}")
     public ResponseEntity<Trip> removeGuest(@PathVariable Long tripId, @PathVariable Long accountId){
