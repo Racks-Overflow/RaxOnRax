69d3f7ad2966b47d6f4938a2b0a3006bdd7ff4da
NicholasWolak <nick.wolak@hotmail.com>
2021-08-29 15:00:22 -0400

    tr

--- src/main/java/com/staxoverflow/demo/repository/BankAccRepo.java
+++ src/main/java/com/staxoverflow/demo/repository/BankAccRepo.java
@@ -2,6 +2,8 @@ package com.staxoverflow.demo.repository;
 
 import com.staxoverflow.demo.domain.BankAccount;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BankAccRepo extends CrudRepository <BankAccount, Long> {
 }
