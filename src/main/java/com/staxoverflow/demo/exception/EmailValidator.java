376383b519be51909f5fa1d414b295473053b21e
NicholasWolak <nick.wolak@hotmail.com>
2021-08-29 16:20:23 -0400

    (feat:nick) email validator/username validator implemented--baseline

--- src/main/java/com/staxoverflow/demo/exception/EmailValidator.java
+++ src/main/java/com/staxoverflow/demo/exception/EmailValidator.java
@@ -5,7 +5,17 @@ import java.util.regex.Pattern;
 
 public interface EmailValidator {
 
-    default boolean validateString(String input) {
-        return input.contains("@");
+    default boolean validateUsername(String input) {
+        Pattern pattern = Pattern.compile("^\\s*\\S+\\s*$");
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+
+    //checks if email is valid --> (must include [some string]@[some domain].com) <-- something like that
+    default boolean isEmailValid(String email) {
+        final Pattern EMAIL_REGEX = Pattern.compile(
+                "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+                Pattern.CASE_INSENSITIVE);
+        return EMAIL_REGEX.matcher(email).matches();
     }
 }
