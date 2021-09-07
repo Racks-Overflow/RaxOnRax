1fe5656bda34af5753b20ab0cbf9b9b061527454
Dee <dee@zipcodes-MacBook-Pro-2.local>
2021-09-06 21:58:52 -0400

    password encoder and web security config set up

--- src/main/java/com/staxoverflow/demo/authentication/UserAccountServices.java
+++ src/main/java/com/staxoverflow/demo/authentication/UserAccountServices.java
@@ -1,10 +1,15 @@
 package com.staxoverflow.demo.authentication;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class UserAccountServices implements UserDetailsService {
+
+
+
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         return null;
--- src/main/java/com/staxoverflow/demo/security/PasswordEncoder.java
+++ src/main/java/com/staxoverflow/demo/security/PasswordEncoder.java
@@ -1,4 +1,13 @@
 package com.staxoverflow.demo.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class PasswordEncoder {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
