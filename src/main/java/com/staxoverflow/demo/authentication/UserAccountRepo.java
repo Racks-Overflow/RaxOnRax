efc400311bae80290cb5f07e29157b90e1538a9e
NicholasWolak <nick.wolak@hotmail.com>
2021-09-07 14:59:45 -0400

    fa

--- src/main/java/com/staxoverflow/demo/authentication/UserAccountRepo.java
+++ src/main/java/com/staxoverflow/demo/authentication/UserAccountRepo.java
@@ -1,12 +1,12 @@
-package com.staxoverflow.demo.authentication;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
-
-@Transactional(readOnly = true)
-@Repository
-public interface UserAccountRepo {
-    Optional<UserAccountDetails> findByEmail(String email);
-}
+//package com.staxoverflow.demo.authentication;
+//
+//import org.springframework.stereotype.Repository;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.Optional;
+//
+//@Transactional(readOnly = true)
+//@Repository
+//public interface UserAccountRepo {
+//    Optional<UserAccountDetails> findByEmail(String email);
+//}
--- src/main/java/com/staxoverflow/demo/security/config/WebSecurityConfig.java
+++ src/main/java/com/staxoverflow/demo/security/config/WebSecurityConfig.java
@@ -1,56 +1,56 @@
-package com.staxoverflow.demo.security.config;
-
-import com.staxoverflow.demo.authentication.UserAccountServices;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    UserAccountServices accountServices; //may have to rebuild all arg constructor if this doesn't work
-
-    @Autowired
-    BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    public WebSecurityConfig() {
-    }
-
-    public WebSecurityConfig(boolean disableDefaults) {
-        super(disableDefaults);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable() //will be enabled later with form authorization
-                .authorizeRequests()
-                .antMatchers("/api/v*/registration/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated().and()
-                .formLogin();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider(){
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(bCryptPasswordEncoder);
-        provider.setUserDetailsService(accountServices);
-        return provider;
-
-    }
-}
+//package com.staxoverflow.demo.security.config;
+//
+//import com.staxoverflow.demo.authentication.UserAccountServices;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Autowired
+//    UserAccountServices accountServices; //may have to rebuild all arg constructor if this doesn't work
+//
+//    @Autowired
+//    BCryptPasswordEncoder bCryptPasswordEncoder;
+//
+//    public WebSecurityConfig() {
+//    }
+//
+//    public WebSecurityConfig(boolean disableDefaults) {
+//        super(disableDefaults);
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable() //will be enabled later with form authorization
+//                .authorizeRequests()
+//                .antMatchers("/api/v*/registration/**")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated().and()
+//                .formLogin();
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(daoAuthenticationProvider());
+//    }
+//
+//    @Bean
+//    public DaoAuthenticationProvider daoAuthenticationProvider(){
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//        provider.setPasswordEncoder(bCryptPasswordEncoder);
+//        provider.setUserDetailsService(accountServices);
+//        return provider;
+//
+//    }
+//}
