dfe934c4f058b4f3c9a9cc278a8d928bd42861c5
NicholasWolak <nick.wolak@hotmail.com>
2021-09-06 22:05:38 -0400

    Authentication

--- src/main/java/com/staxoverflow/demo/authentication/UserAccountServices.java
+++ src/main/java/com/staxoverflow/demo/authentication/UserAccountServices.java
@@ -1,12 +1,25 @@
 package com.staxoverflow.demo.authentication;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserAccountServices implements UserDetailsService {
+
+    @Autowired
+    private final UserAccountRepo repo;
+
+    public UserAccountServices(UserAccountRepo repo) {
+        this.repo = repo;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String s)
+            throws UsernameNotFoundException {
+        return repo.findByEmail(s)
+                .orElseThrow(() -> new UsernameNotFoundException("Username does not exist."));
     }
 }
