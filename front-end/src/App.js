3268b681694d409b7c208f73746647bf211cd500
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-09-09 12:44:15 -0400

    refractoring a bit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Header from "./components/layout/Header";
 import SignInForm from "./components/Forms/LoginForm";
 import 'react-app-polyfill/stable';
+import Footer from "./components/layout/Footer"
 
 
 
@@ -43,8 +44,9 @@ export default function App() {
 	return(
 		<section>
 			<div><Header /></div>
-			
+			<div><Footer /></div>
 		</section>
+
 	)
 }
 	
